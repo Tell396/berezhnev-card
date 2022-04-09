26fc0c230b47cd581ebf72e2d40ed751ea25e443
Tell396 <vova21473@gmail.com>
2022-04-09 22:05:24 +1000

    Story #2. Icons, About text - was be added.

---
+++ components/Icons.jsx
@@ -0,0 +1,28 @@
+import { FaGithub, FaRegFile, FaBlackTie } from 'react-icons/fa';
+import { IconContext } from "react-icons";
+
+import { Center, Flex, Text } from '@chakra-ui/react'
+
+import Link from 'next/link'
+
+export default function Icons() {
+    return (
+        <Center>
+            <Flex>
+                <IconContext.Provider value={{ size: 25 }} >
+                    <Text mr={3} >
+                        <a href="https://github.com/Tell396" target="_blank" rel="noopener noreferrer">
+                            <FaGithub />
+                        </a>
+                    </Text>
+
+                    <Text mr={3}>
+                        <a href="https://berezhnev-card-lt7t5wfc9-tell396.vercel.app/" target="_blank" rel="noopener noreferrer">
+                            <FaBlackTie />
+                        </a>
+                    </Text>
+                </IconContext.Provider>
+            </Flex>
+        </Center >
+    )
+}
